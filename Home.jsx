53fdb49e765848e478b9b3ae77a0d152d40479b4
Kanishka2406 <kanishkanithya266@gmail.com>
2025-10-08 21:59:30 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page-container">
+      <h1>Welcome to Alumni Management System</h1>
+      <p>Connect with your alumni, manage events, and view profiles easily.</p>
+    </div>
+  );
+}
+
+export default Home;
